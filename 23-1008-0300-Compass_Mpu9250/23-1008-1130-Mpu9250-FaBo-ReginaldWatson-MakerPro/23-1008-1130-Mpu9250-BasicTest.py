2e3ac31832d17c08efdbe4bb5d44c3d78a1b0973
eande-jasonc1025 <eande.jasonc.@gmail.com>
2023-10-11 16:04:51 -0700

    23-1011-1600 Checkin

---
+++ 23-1008-0300-Compass_Mpu9250/23-1008-1130-Mpu9250-FaBo-ReginaldWatson-MakerPro/23-1008-1130-Mpu9250-BasicTest.py
@@ -0,0 +1,30 @@
+# jwc 23-1008-1130 https://maker.pro/raspberry-pi/tutorial/how-to-interface-an-imu-sensor-with-a-raspberry-pi
+
+import FaBo9Axis_MPU9250
+import time
+import sys
+
+mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+
+try:
+    while True:
+        accel = mpu9250.readAccel()
+        print (" ax = " , ( accel['x'] ))
+        print (" ay = " , ( accel['y'] ))
+        print (" az = " , ( accel['z'] ))
+
+        gyro = mpu9250.readGyro()
+        print (" gx = " , ( gyro['x'] ))
+        print (" gy = " , ( gyro['y'] ))
+        print (" gz = " , ( gyro['z'] ))
+
+        mag = mpu9250.readMagnet()
+        print (" mx = " , ( mag['x'] ))
+        print (" my = " , ( mag['y'] ))
+        print (" mz = " , ( mag['z'] ))
+        print
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    sys.exit()
